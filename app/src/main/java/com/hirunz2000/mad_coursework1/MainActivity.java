320f7ddc406cad160370c96b87b41cb056c8692a
Hirunz <54896502+Hirunz@users.noreply.github.com>
2021-03-03 23:17:11 +0530

    Added home page design & app theme

--- app/src/main/java/com/hirunz2000/mad_coursework1/MainActivity.java
+++ app/src/main/java/com/hirunz2000/mad_coursework1/MainActivity.java
@@ -3,12 +3,32 @@ package com.hirunz2000.mad_coursework1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static double delay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+    public void onButton1(View view) {
+    }
+
+    public void onButton2(View view) {
+    }
+
+    public void onButton3(View view) {
+    }
+
+    public void onButton4(View view) {
     }
 }
