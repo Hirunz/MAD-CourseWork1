f54f27b008889fcb61c260de21247001696bd502
Hirunz <54896502+Hirunz@users.noreply.github.com>
2021-03-04 01:00:14 +0530

    added 40 car photos

---
+++ app/src/main/java/com/hirunz2000/mad_coursework1/Advanced.java
@@ -0,0 +1,14 @@
+package com.hirunz2000.mad_coursework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Advanced extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced);
+    }
+}
--- app/src/main/java/com/hirunz2000/mad_coursework1/MainActivity.java
+++ app/src/main/java/com/hirunz2000/mad_coursework1/MainActivity.java
@@ -2,33 +2,36 @@ package com.hirunz2000.mad_coursework1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static double delay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
     }
 
     public void onButton1(View view) {
+        Intent intent=new Intent(this,Car_Make.class);
+        startActivity(intent);
     }
 
     public void onButton2(View view) {
+        Intent intent=new Intent(this,Hints.class);
+        startActivity(intent);
     }
 
     public void onButton3(View view) {
+        Intent intent=new Intent(this,IdentifyCarImg.class);
+        startActivity(intent);
     }
 
     public void onButton4(View view) {
+        Intent intent=new Intent(this,Advanced.class);
+        startActivity(intent);
     }
 }
